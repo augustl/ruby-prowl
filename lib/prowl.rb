9282e89facbd4b35ff4911c593609b7f9e23b0d2
August Lilleaas <august@augustl.com>
2016-01-17 21:24:57 +0100

    Remove extraneous whitespace

--- lib/prowl.rb
+++ lib/prowl.rb
@@ -7,11 +7,11 @@ class Prowl
   class MissingAPIKey < RuntimeError; end
   class TooManyAPIKeys < RuntimeError; end
   class PriorityOutOfRange < RuntimeError; end
-  
+
   API_URL = "https://api.prowlapp.com/publicapi"
   MAX_API_KEYS = 5
   PRIORITY_RANGE = -2..2
-  
+
   def initialize(defaults = {})
     @defaults = defaults
   end
@@ -20,56 +20,56 @@ class Prowl
     @proxy_addr = addr
     @proxy_port = port
   end
-  
+
   def add(params = {})
     perform("add", params)
   end
-  
+
   # Modify this instance's defaults
   def defaults(params)
     @defaults = @defaults.merge(params)
   end
-  
+
   def valid?
     @valid ||= (perform("verify") == 200)
   end
-  
+
   # Utility function that creates an instance and sends a prowl
   def self.add(params = {})
     new(params).add
   end
-  
+
   # Utility function to verify API keys
   def self.verify(apikey)
     new({:apikey => apikey}).valid?
   end
-  
+
   private
-  
+
   def perform(action, params = {})
     # Merge the default params with any custom ones
     unless !@defaults
       params = @defaults.merge(params)
     end
-    
+
     if !params[:apikey] || (params[:apikey].is_a?(Array) && params[:apikey].size < 1)
       raise MissingAPIKey
     end
-    
+
     # Raise an exception if we're trying to use more API keys than allowed for this action
     if params[:apikey].is_a?(Array) && ((action == "verify" && params[:apikey].size > 1) || params[:apikey].size > MAX_API_KEYS)
       raise TooManyAPIKeys
     end
-    
+
     if params[:priority] && !PRIORITY_RANGE.include?(params[:priority])
       raise PriorityOutOfRange
     end
-    
+
     # If there are multiple API Keys in an array, merge them into a comma-delimited string
     if params[:apikey].is_a?(Array)
       params[:apikey] = params[:apikey].join(",")
     end
-    
+
     uri = URI.parse("#{API_URL}/#{action}")
 
     proxy = Net::HTTP::Proxy(@proxy_addr, @proxy_port)
@@ -90,7 +90,7 @@ end
 # For me and my good friend friend Textmate.
 if __FILE__ == $0
   api_key = "change me"
-  
+
   p Prowl.add(:apikey => api_key, :application => "Fishes", :event => "silly", :description => "Awwawaw.", :priority => 1)
   p Prowl.verify(api_key)
 end
